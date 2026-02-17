a1605fa1cb3c7b9cb6b3850d07c82a49b1114553
Khizar <khizarnawab1@outlook.com>
2026-02-17 05:17:15 +0500

    feat: keyword search with stemming

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -5,9 +5,15 @@ DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOP_WORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
 
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data["movies"]
+
+
+def load_stop_words() -> list[str]:
+    with open(STOP_WORDS_PATH, "r", encoding="utf-8") as f:
+        return f.read().splitlines()
